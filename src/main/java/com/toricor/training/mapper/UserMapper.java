b5379383c7adae7a584ca008f6b2d492ae590a42
toriyabe <toriyabe@mfpc243.local>
2017-10-02 14:28:17 +0900

    userのallはjson配列表示できた

--- src/main/java/com/toricor/training/mapper/UserMapper.java
+++ src/main/java/com/toricor/training/mapper/UserMapper.java
@@ -4,6 +4,9 @@ import com.toricor.training.domain.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface UserMapper {
@@ -12,6 +15,9 @@ public interface UserMapper {
     @Options(useGeneratedKeys = true)
     void insert(User user);
 
+    @Select("SELECT id, name FROM user WHERE id = #{id}")
     User select(int id);
 
+    @Select("SELECT id, name FROM user")
+    List<User> selectAll();
 }
