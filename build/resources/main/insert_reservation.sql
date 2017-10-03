96ae0b94d299a94b586fb8b9c2c57146ebb89df9
toriyabe <toriyabe@mfpc243.local>
2017-10-03 09:42:15 +0900

    gitignore整理、不要ファイル排除

--- build/resources/main/insert_reservation.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO reservation VALUES(1, 1, 1);
-INSERT INTO reservation VALUES(2, 1, 2);
-INSERT INTO reservation VALUES(3, 2, 1);
--- build/resources/main/insert_user.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO user VALUES(1, "name1");
-INSERT INTO user VALUES(2, "name2");
-INSERT INTO user VALUES(3, "name3");
-INSERT INTO user VALUES(4, "name4");
