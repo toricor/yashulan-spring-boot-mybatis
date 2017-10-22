d52c541ed5889b1cae3a6dc755d046110825e853
toricor <irotoridoritoriyaben@yahoo.co.jp>
2017-10-22 20:07:16 +0900

    schema変更、data追加

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,25 +1,34 @@
+CREATE TABLE IF NOT EXISTS restaurant (
+    id              INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    station         VARCHAR(255) NOT NULL,
+    name            VARCHAR(255) NOT NULL UNIQUE,
+    genre           VARCHAR(255) NOT NULL,
+    lunch_or_dinner VARCHAR(25),
+    budget_lower    INTEGER,
+    budget_upper    INTEGER,
+    star            FLOAT NOT NULL,
+    good            VARCHAR(255) NOT NULL,
+    tabelog         VARCHAR(255),
+    created_at      datetime NOT NULL,
+    updated_at      TIMESTAMP on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    INDEX(`name`),
+    INDEX(`star`)
+) ENGINE=InnoDB DEFAULT CHARACTER SET utf8;
+
 CREATE TABLE IF NOT EXISTS user (
-    id       INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    name     VARCHAR(255) NOT NULL UNIQUE
-);
+    id           INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name         VARCHAR(255) NOT NULL UNIQUE,
+    password     VARCHAR(255) NOT NULL,
+    created_at   VARCHAR(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARACTER SET utf8;
 
-CREATE TABLE IF NOT EXISTS event (
-    id               INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    title            VARCHAR(255) NOT NULL,
-    description      VARCHAR(255) NOT NULL,
-    datetime         DATETIME NOT NULL,
-    publish          DATETIME NOT NULL,
-    author           INTEGER NOT NULL,
-    place            VARCHAR(255) NOT NULL,
-    participants     VARCHAR(255) NOT NULL,
-    max_participants VARCHAR(255) NOT NULL,
-    FOREIGN KEY(author) REFERENCES user(id)
-);
+CREATE TABLE IF NOT EXISTS favorite (
+    id            INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    user_id       INTEGER NOT NULL,
+    restaurant_id INTEGER NOT NULL,
+    created_at    VARCHAR(255) NOT NULL,
 
-CREATE TABLE IF NOT EXISTS reservation (
-    id        INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    user_id   INTEGER NOT NULL,
-    event_id  INTEGER NOT NULL,
-    FOREIGN KEY(user_id) REFERENCES user(id),
-    FOREIGN KEY(event_id) REFERENCES event(id)
-);
+    INDEX `target_data` (`user_id`, `restaurant_id`),
+    foreign key(user_id) references user(id),
+    foreign key(restaurant_id) references restaurant(id)
+) ENGINE=InnoDB DEFAULT CHARACTER SET utf8;
